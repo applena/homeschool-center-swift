a6648c12d240bd0d35cf3665ad2fa8989cc41a0f
Lena Eivy <applena@gmail.com>
2023-03-06 11:01:38 -0800

    Initial Commit

---
+++ homeschool-center-swift/homeschool_center_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeschool_center_swiftApp.swift
+//  homeschool-center-swift
+//
+//  Created by Lena Eivy on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct homeschool_center_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
