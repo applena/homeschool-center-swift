757688211f4906f55aafc03f4f729ece3564128d
Lena Eivy <applena@gmail.com>
2023-03-24 12:22:08 -0700

    working collect student info page with conditional rendering

--- homeschool-center-swift/ContentView.swift
+++ homeschool-center-swift/ContentView.swift
@@ -7,69 +7,55 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
-    @State private var name = ""
-    @State private var gradeString = ""
+    @State var name: String = ""
+    @State var gradeString: String = ""
     @State var noStudentInfo: Bool = true
 
-    
     var body: some View {
-        NavigationView{
-            VStack(alignment: .center){
-                Text("Welcome to Homeschool Center")
-                    .font(.system(size: 33))
-                    .multilineTextAlignment(.center)
-                TextField("Student Name", text: $name)
-                    .padding()
-                TextField("Student Grade", text: $gradeString)
-                    .padding()
-                
-                Button(action: saveStudentInfo) {
-                    Text("Save")
-                }
-                NavigationLink(destination: SecondView(), isActive: $noStudentInfo) {
-                    EmptyView()
-                }
-               
+        VStack {
+            if noStudentInfo {
+                CollectStudentInfoView()
+            } else {
+                DashboardView()
             }
         }
-//        .onAppear {
-//                    // Check if we have user info
-//            doesNotHaveStudentInfo = ((UserDefaults.standard.dictionary(forKey: "StudentInfo")?.isEmpty) != nil)
-//                }
         .onAppear {
-            // Check if user is logged in
-            noStudentInfo = !UserDefaults.standard.bool(forKey: "studentInfo")
-                }
+            if let dictionary = UserDefaults.standard.dictionary(forKey: "studentInfo"), !dictionary.isEmpty {
+                noStudentInfo = false
+            }
+        }
     }
-    
+
+    func CollectStudentInfoView() -> some View  {
+        VStack {
+            TextField("Student Name", text: $name).padding()
+            TextField("Student Grade", text: $gradeString).padding()
+
+            Button(action: saveStudentInfo) {
+                Text("Save")
+            }
+        }
+    }
+
+    func DashboardView() -> some View {
+        Text("The dictionary is not empty.")
+    }
+
     func saveStudentInfo() {
+        print("user defaults.standard \(String(describing: UserDefaults.standard.dictionary(forKey: "studentInfo")))")
         if let grade = Int(gradeString) {
             // Save student info to UserDefaults
             var studentInfo = UserDefaults.standard.dictionary(forKey: "studentInfo") as? [String: Int] ?? [String: Int]()
             studentInfo[name] = grade
             UserDefaults.standard.setValue(studentInfo, forKey: "studentInfo")
-            self.noStudentInfo = true
+            self.noStudentInfo = false
         } else {
             // Display error message
             print("ERROR - something went wrong")
         }
-        print("user defaults.standard \(String(describing: UserDefaults.standard.dictionary(forKey: "studentInfo")))")
     }
 }
-
-struct SecondView: View {
-    var body: some View {
-        VStack {
-            Text("This is the second view")
-                .padding()
-            .padding()
-        }
-        .navigationTitle("Second View")
-    }
-}
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
